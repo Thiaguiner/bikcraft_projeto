257fb54330f01801e3e38a2f9e8835eca343d143
Thiaguiner <tcarneiro990@gmail.com>
2023-12-08 10:41:23 -0300

    adicionando js nas imagens

--- js/script.js
+++ js/script.js
@@ -41,3 +41,20 @@ function eventosPerguntas(pergunta) {
   pergunta.addEventListener("click", ativarPergunta);
 }
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicleta
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+  if(media){
+  galeriaContainer.prepend(img);
+}
+}
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
